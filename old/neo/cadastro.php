785eff7455c8b8826e223c7935d78747e907ee97
diegosieg <diegosieg@gmail.com>
2013-08-15 22:28:02 -0300

    removing old documents of the project and some rubish -- final commit

--- old/neo/cadastro.php
+++
@@ -1,7 +0,0 @@
-<div id="textos5">
-
-<iframe src="http://api.neofluxo.net/users/sign_up" width="100%" height="600px" style="border:0">
-  <p><a href="http://api.neofluxo.net/users/sign_up">Cadastre-se</a></p>
-</iframe>
-
-</div>
--- old/neo/en/desenv.php
+++
@@ -1,3 +0,0 @@
-<div id="textos4">
-<p>conteúdo explicando as chamadas e funcionamento da API --- falta fazer. ok?</p>
-</div>
